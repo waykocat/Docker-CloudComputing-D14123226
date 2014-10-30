522e012807d227fab2d005aaffa2da97f278cf21
albert <albert.navarro.sanchez@gmail.com>
2014-10-30 17:37:02 +0200

    new container with listmethod

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask import request
+from os import listdir
+from os.path import isfile, join
+import sys
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,3 +18,11 @@ def upload_file():
     return '',201
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
+@app.route('/ListFiles')
+def list_files():
+    onlyfiles = [f for f in listdir("./uploads/") if isfile(join("./uploads/",f))]
+    listToReturn = ""
+    for i in onlyfiles:
+        listToReturn = listToReturn + i + "\n"
+    return listToReturn
