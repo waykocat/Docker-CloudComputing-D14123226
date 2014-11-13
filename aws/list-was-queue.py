fc7840a00ab2f30e2706675c4f4158de2dacf925
albert <albert.navarro.sanchez@gmail.com>
2014-11-13 18:16:13 +0200

    newscript

--- aws/list-was-queue.py
+++ aws/list-was-queue.py
@@ -9,8 +9,39 @@ from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
 
-conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
+conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAIR7EH3TNSTDUCWKA', aws_secret_access_key='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')
 
-rs = conn.get_all_queues()
-for q in rs:
-	print q.id
+#this line creates a queue
+newQueue = conn.create_queue("queue4Messages")
+
+#creating messages
+
+#we get the queue where we want to write in
+
+#q = conn.get_all_queues()
+q = conn.get_queue('queue4Messages')
+
+#then we create the message object
+m = Message()
+
+ #we want to create 100messages, 
+strmessage = "That message "
+
+
+for i in range(1,100):
+    newM = strmessage + str(i) 
+    m.set_body('message')
+    q.write(m)
+
+rs = q.get_messages()
+
+m = rs[5]
+messagereceived = m.get_body()
+print messagereceibed
+
+
+#deleting all the messages
+for i in range(1,100):
+    m = rs[i]
+    q.delete_message(m)
+
